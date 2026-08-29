883321293b388fe29d0a7e3378c63a9cac11f6f2
soenneker <jake@soenneker.com>
2026-08-29 14:51:16 -0500

    Apply interface XML documentation conventions

--- src/Soenneker.Validators.Email.Syntax/EmailSyntaxValidator.cs
+++ src/Soenneker.Validators.Email.Syntax/EmailSyntaxValidator.cs
@@ -4,7 +4,7 @@ using Soenneker.Validators.Email.Syntax.Abstract;
 
 namespace Soenneker.Validators.Email.Syntax;
 
-///<inheritdoc cref="IEmailSyntaxValidator"/>
+/// <inheritdoc cref="IEmailSyntaxValidator"/>
 public sealed class EmailSyntaxValidator : Validator.Validator, IEmailSyntaxValidator
 {
     public EmailSyntaxValidator(ILogger<EmailSyntaxValidator> logger) : base(logger)
