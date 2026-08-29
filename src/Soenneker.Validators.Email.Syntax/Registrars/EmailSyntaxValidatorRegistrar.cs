d8a292e4714b6abad346af9cec0d79559779b989
soenneker <jake@soenneker.com>
2026-08-29 14:17:26 -0500

    Improve public API XML documentation

--- src/Soenneker.Validators.Email.Syntax/Registrars/EmailSyntaxValidatorRegistrar.cs
+++ src/Soenneker.Validators.Email.Syntax/Registrars/EmailSyntaxValidatorRegistrar.cs
@@ -12,6 +12,8 @@ public static class EmailSyntaxValidatorRegistrar
     /// <summary>
     /// Adds <see cref="IEmailSyntaxValidator"/> as a singleton service.
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddEmailSyntaxValidatorAsSingleton(this IServiceCollection services)
     {
         services.TryAddSingleton<IEmailSyntaxValidator, EmailSyntaxValidator>();
@@ -22,6 +24,8 @@ public static class EmailSyntaxValidatorRegistrar
     /// <summary>
     /// Adds <see cref="IEmailSyntaxValidator"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddEmailSyntaxValidatorAsScoped(this IServiceCollection services)
     {
         services.TryAddScoped<IEmailSyntaxValidator, EmailSyntaxValidator>();
